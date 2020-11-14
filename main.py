f27d7dfd09d6848ff983edaf983946f454fa71ea
Liam <liampeters00@icloud.com>
2020-11-14 14:22:15 +0000

    Added current files

---
+++ main.py
@@ -0,0 +1,13 @@
+is_Alex = int(input('Are you Alex? 0 = yes, 1 = no:     '))
+if is_Alex == 0:
+    you_a_bitch = int(input('Are you a bitch? 0 = yes, 1 = no:    '))
+    if you_a_bitch == 0:
+        print('hello Alex you a bitch')
+    else:
+        print('Dont lie alex you are a bitch')
+elif is_Alex == 1:
+    print("you aint alex so you aint a bitch")
+else:
+    print('Please put 1 or 0')
+
+
